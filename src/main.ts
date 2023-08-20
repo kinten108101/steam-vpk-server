e9b683b0af4d31aa91921b92914ecb023d7bb501
kinten108101 <kinten108101@protonmail.com>
2023-08-21 03:27:35 +0700

    Enabled debug logs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import 'gi://Gdk?version=4.0';
 import 'gi://Gtk?version=4.0';
 import 'gi://Soup?version=3.0';
+import GLib from 'gi://GLib';
 
 import Server from './server.js';
 
 export function main(argv: string[] | null): number {
+  GLib.log_set_debug_enabled(true);
   return Server().run(argv);
 }
