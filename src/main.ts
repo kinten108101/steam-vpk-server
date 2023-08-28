a72081b80366f736fda8614c38689926fc90e171
kinten108101 <kinten108101@protonmail.com>
2023-08-28 22:01:46 +0700

    Own promisify initialization step

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import 'gi://Gtk?version=4.0';
 import 'gi://Soup?version=3.0';
 import GLib from 'gi://GLib';
 
+import './promisify.js';
 import Server from './server.js';
 
 export function main(argv: string[] | null): number {
---
+++ src/promisify.ts
@@ -0,0 +1,97 @@
+import Gio from 'gi://Gio';
+import Soup from 'gi://Soup';
+
+Gio._promisify(
+  Gio.File.prototype,
+  'query_info_async',
+  'query_info_finish',
+);
+
+Gio._promisify(
+  Gio.FileOutputStream.prototype,
+  'query_info_async',
+  'query_info_finish',
+);
+Gio._promisify(
+  Gio.File.prototype,
+  'trash_async',
+  'trash_finish',
+);
+
+Gio._promisify(
+  Soup.Session.prototype,
+  'send_async',
+  'send_finish',
+);
+
+Gio._promisify(
+  Gio.File.prototype,
+  'replace_async',
+  'replace_finish',
+);
+
+Gio._promisify(
+  Gio.InputStream.prototype,
+  'read_all_async',
+  'read_all_finish',
+);
+
+Gio._promisify(
+  Gio.OutputStream.prototype,
+  'write_async',
+  'write_finish',
+);
+
+Gio._promisify(
+  Gio.OutputStream.prototype,
+  'write_bytes_async',
+  'write_bytes_finish',
+);
+
+Gio._promisify(
+  Soup.Session.prototype,
+  'send_and_splice_async',
+  'send_and_splice_finish',
+);
+
+Gio._promisify(
+  Gio.OutputStream.prototype,
+  'flush_async',
+  'flush_finish',
+);
+
+Gio._promisify(
+  Gio.InputStream.prototype,
+  'close_async',
+  'close_finish',
+);
+
+Gio._promisify(
+  Gio.OutputStream.prototype,
+  'close_async',
+  'close_finish',
+);
+
+Gio._promisify(
+  Gio.InputStream.prototype,
+  'read_bytes_async',
+  'read_bytes_finish',
+);
+
+Gio._promisify(
+  Gio.InputStream.prototype,
+  'read_all_async',
+  'read_all_finish',
+);
+
+Gio._promisify(
+  Gio.OutputStream.prototype,
+  'splice_async',
+  'splice_finish',
+);
+
+Gio._promisify(
+  Gio.File.prototype,
+  'move_async',
+  'move_finish',
+);
